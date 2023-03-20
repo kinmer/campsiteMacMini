405b9464a199e6d1ac0118b8df4d36f664b5a867
yongZhang <kinmer7@gmail.com>
2023-03-20 16:12:31 +0800

    Network Info

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -6,6 +6,9 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import * as ImagePicker from 'expo-image-picker';
 import { baseUrl } from '../shared/baseUrl';
 import logo from '../assets/images/logo.png';
+import * as ImageManipulator from 'expo-image-manipulator';
+
+
 
 const LoginTab = ({ navigation }) => {
     const [username, setUsername] = useState('');
@@ -146,12 +149,37 @@ const RegisterTab = () => {
                 allowsEditing: true,
                 aspect: [1, 1]
             });
-            if (capturedImage.assets) {
-                console.log(capturedImage.assets[0]);
-                setImageUrl(capturedImage.assets[0].uri);
+            if (!capturedImage.cancelled) {
+                console.log(capturedImage);
+                processImage(capturedImage.uri);
             }
         }
     };
+    const getImageFromGallery = async () => {
+        const mediaLibraryPermissions =
+            await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+        if (mediaLibraryPermissions.status === 'granted') {
+            const capturedImage = await ImagePicker.launchImageLibraryAsync({
+                allowsEditing: true,
+                aspect: [1, 1]
+            });
+            if (capturedImage.assets) {
+                console.log(capturedImage.assets[0]);
+                processImage(capturedImage.assets[0].uri);
+            }
+        }
+    };
+
+    const processImage = async (imgUri) => {
+        imageActions = [{ resize: { height: 400 }}]
+        const processedImage = await ImageManipulator.manipulateAsync(
+            imgUri,
+            imageActions
+        );
+        console.log(processedImage);
+        setImageUrl(processedImage.uri)
+    }
 
     return (
         <ScrollView>
@@ -163,6 +191,7 @@ const RegisterTab = () => {
                         style={styles.image}
                     />
                     <Button title='Camera' onPress={getImageFromCamera} />
+                    <Button title='Gallery' onPress={getImageFromGallery} />
                 </View>
                 <Input
                     placeholder='Username'
